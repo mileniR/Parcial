825928121f279dbf2dd1446806a72d47f416019d
unknown <milreyesca@uch.pe>
2024-04-24 23:19:49 -0500

    first commit

--- js/conductor.js
+++ js/conductor.js
@@ -5,31 +5,46 @@ document.addEventListener('DOMContentLoaded', function() {
     // Array de objetos simulando los datos de los viajes
     const viajes = [
         { usuario: 'Harry Styles', costo: 200 },
-        { usuario: 'Taylor Switf', costo: 150 },
+        { usuario: 'Taylor Swift', costo: 150 },
         { usuario: 'Olivia Rogrigo', costo: 3000 },
         { usuario: 'Louis Tomlinson', costo: 390 },
         { usuario: 'Zayn Malik', costo: 600 },
         { usuario: 'Niall Horan', costo: 450 },
         { usuario: 'Liam Payne', costo: 850 },
-        { usuario: 'Sabrina Carpentes', costo: 570 },
+        { usuario: 'Sabrina Carpenter', costo: 570 },
         { usuario: 'Travis Scott', costo: 340 },
         { usuario: 'Joe Jonas', costo: 970 },
     ];
 
     // Función para mostrar la lista de usuarios que más abordaron el vehículo
-    function mostrarUsuariosAbordaron() {
-        const usuarios = viajes.map(viaje => viaje.usuario).join(', ');
-        usuariosAbordaron.textContent = usuarios;
+    const personasElement = document.getElementById("usuariosAbordaron");
+    
+    function writePersona() {
+        let contenido = "";
+        viajes.forEach(persona => {
+            const template = `
+                <div class="persona">
+                    <div class="data">
+                    <h4>${persona.usuario}</h4>
+                    </div>
+                    <div class="costo">
+                    <h4>S/. ${persona.costo}</h4>
+                    </div>
+                </div>    
+            `;
+            contenido = contenido + template;
+        });
+        personasElement.innerHTML = contenido;
+    
     }
 
     // Función para calcular el monto total de los viajes realizados
     function calcularMontoTotal() {
         const montoTotal = viajes.reduce((total, viaje) => total + viaje.costo, 0);
-        montoTotalElement.textContent = `Monto Total =${montoTotal} soles`;
+        montoTotalElement.textContent = `Monto Total = ${montoTotal} soles`;
     }
 
-    // Mostrar la lista de usuarios y el monto total al cargar la página
-    mostrarUsuariosAbordaron();
+    writePersona();
     calcularMontoTotal();
 });
 
